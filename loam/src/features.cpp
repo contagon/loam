a18f1eef84a416f11d8ab8e63a47c5e5202c4187
Easton Potokar <contagon6@gmail.com>
2025-02-22 15:09:04 -0500

    Small tweak to alternative curvature defs

--- loam/src/features.cpp
+++ loam/src/features.cpp
@@ -46,14 +46,14 @@ bool markOccludedInvalid(const size_t& idx, const double& point_range, const dou
   if (next_point_range - point_range > params.occlusion_thresh) {  // Case 1
     for (size_t n = 1; n <= params.neighbor_points; n++) mask[idx + n] = false;
     // If we're using nearest neighbors, features on the close side are also NOT planar
-    if (params.curvature_type == FeatureExtractionParams::Curvature::EIGEN_NN) {
+    if (params.curvature_type != FeatureExtractionParams::Curvature::LOAM) {
       for (size_t n = 0; n <= params.neighbor_points; n++) mask[idx - n] = false;
     }
     return true;
   } else if (point_range - next_point_range > params.occlusion_thresh) {  // Case 2
     for (size_t n = 0; n < params.neighbor_points; n++) mask[idx - n] = false;
     // If we're using nearest neighbors, features on the close side are also NOT planar
-    if (params.curvature_type == FeatureExtractionParams::Curvature::EIGEN_NN) {
+    if (params.curvature_type != FeatureExtractionParams::Curvature::LOAM) {
       for (size_t n = 0; n <= params.neighbor_points; n++) mask[idx + n] = false;
     }
     return true;
